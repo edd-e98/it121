1108c45085440f2581a235ba83960c918ac9b06d
Eddie <87498041+edd-e98@users.noreply.github.com>
2023-01-07 15:34:52 -0800

    Adding week 1's about me homework

---
+++ week1/script.js
@@ -0,0 +1,7 @@
+// This defines some variables for us to use in our index.html
+const firstName = "Eddie";
+const lastName = "Melton";
+const yearsOfStudy = "1.5";
+const goal = "build websites for local businesses";
+
+const greeting = `Hi there! My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} years and plan to ${goal} after completing my courses.`;
